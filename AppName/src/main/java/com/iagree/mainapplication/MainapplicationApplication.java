5cb4b651e23020bf3f0db4cc5dd78a1af2ec168c
ram5353 <ramkiran5353@gmail.com>
2019-11-14 07:49:15 +0530

    first commit

---
+++ AppName/src/main/java/com/iagree/mainapplication/MainapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.iagree.mainapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainapplicationApplication.class, args);
+    }
+
+}
---
+++ AppName/src/test/java/com/iagree/mainapplication/MainapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iagree.mainapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainapplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
